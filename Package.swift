96e366faaf0ef7fc239a088222b24d78b8999053
woolie <steven_woolgar@woolsoft.com>
2021-03-30 12:09:15 -0700

    Now dependant on music-notation package because we are adding extensions

--- Package.swift
+++ Package.swift
@@ -2,12 +2,28 @@
 import PackageDescription
 
 let package = Package(
-    name: "music-notation-import-guitarpro",
-    platforms: [.macOS(.v10_15)],
-    products: [.library(name: "MusicNotationImportGuitarPro", targets: ["MusicNotationImportGuitarPro"])],
-	dependencies: [.package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "5.0.2")],
-    targets: [
-		.target(name: "MusicNotationImportGuitarPro", dependencies: ["SWXMLHash"], path: "Sources", exclude: ["images"]),
-        .testTarget(name: "MusicNotationImportGuitarProTests", dependencies: ["MusicNotationImportGuitarPro"])
-    ]
+	name: "music-notation-import-guitarpro",
+	platforms: [.macOS(.v10_15)],
+	products: [.library(name: "MusicNotationImportGuitarPro", targets: ["MusicNotationImportGuitarPro"])],
+
+	dependencies: [
+		.package(url: "https://github.com/music-notation-swift/music-notation.git", from: "0.2.5"),
+		.package(url: "https://github.com/drmohundro/SWXMLHash.git", from: "5.0.2"),
+	],
+
+	targets: [
+		.target(
+			name: "MusicNotationImportGuitarPro",
+			dependencies: [
+				"SWXMLHash",
+				.product(name: "MusicNotation", package: "music-notation")
+			],
+			path: "Sources",
+			exclude: ["images"]
+		),
+		.testTarget(
+			name: "MusicNotationImportGuitarProTests",
+			dependencies: ["MusicNotationImportGuitarPro"]
+		)
+	]
 )
