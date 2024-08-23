00dbf3c2326c1980152cc9eba95545e419dddd1d
Steven Woolgar <steven_woolgar@woolsoft.com>
2024-08-23 10:43:06 -0700

    Started adding tests to PartConfiguration

---
+++ Tests/MusicNotationImportGuitarProTests/PartConfigurationTests.swift
@@ -0,0 +1,33 @@
+//
+//	PartConfigurationTests.swift
+//	music-notation-import-guitarpro-tests
+//
+//	Created by Steven Woolgar on 2021-04-01.
+//	Copyright © 2021 Steven Woolgar. All rights reserved.
+//
+
+import Foundation
+@testable import MusicNotationImportGuitarPro
+import Testing
+
+@Suite final class PartConfigurationTests {
+    let data = Data([UInt8](arrayLiteral:
+        0x00, 0x00, 0x00, 0x06,
+        0x00, 0x00, 0x00, 0x00,
+        0x05, 0x02, 0x01, 0x04,
+        0x08, 0x08, 0x00, 0x00,
+        0x00, 0x00, 0x01, 0x02,
+        0x00, 0x00, 0x00, 0x00,
+        0x01, 0x01, 0x00, 0x00,
+        0x00, 0x00, 0x01, 0x04,
+        0x00, 0x00, 0x00, 0x00,
+        0x01, 0x08, 0x00, 0x00,
+        0x00, 0x00, 0x01, 0x08,
+        0x00, 0x00, 0x00, 0x05
+    ))
+
+    @Test func parse() async throws {
+        let partConfigs = try PartConfiguration.partConfigurationArrayFrom(data: data)
+        #expect(partConfigs.count == 8)
+    }
+}
