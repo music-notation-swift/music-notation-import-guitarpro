ef5a5c33ef958616c0145e8d3df7a82710dcbb70
Steven Woolgar <steven_woolgar@woolsoft.com>
2024-08-27 14:44:37 -0700

    Added project .editorconfig file

--- Sources/MusicNotationExtensions/MusicNotation+Part.swift
+++ Sources/MusicNotationExtensions/MusicNotation+Part.swift
@@ -1,5 +1,5 @@
 //
-//	Staff.swift
+//	MusicNotation+Part.swift
 //	music-notation-import-guitarpro
 //
 //	Created by Steven Woolgar on 2021-03-30.
@@ -10,8 +10,25 @@ import MusicNotation
 
 /// An extension to create MusicNotation.Staff from a Track struct.
 /// This is a function of the GP7 importer and NOT of the Staff struct.
-public extension MusicNotation.Part {
+extension MusicNotation.Part {
 	init(with track: Track) {
-		self.init(name: track.name, shortName: track.shortName, staves: [])
+		let staves: [MusicNotation.Staff] = track.staves.map {
+			MusicNotation.Staff(with: $0)
+		}
+
+		self.init(name: track.name, shortName: track.shortName, staves: staves)
+	}
+}
+
+extension MusicNotation.Staff {
+	init(with staff: Staff) {
+		self.init(
+			clef: .treble,
+			instrument: .init(name: "instrument"),
+			measure: [Measure(
+				timeSignature: MusicNotation.TimeSignature(numerator: 4, denominator: 4, tempo: 120),
+				key: MusicNotation.Key(noteLetter: .c)
+			)]
+		)
 	}
 }
